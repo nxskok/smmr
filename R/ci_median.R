7e3220dcf8db67efc5f5acf142ac2b59fe8c4b90
nxskok <nxskok@gmail.com>
2017-06-16 17:57:09 -0400

    try to get rid of check messages

--- R/ci_median.R
+++ R/ci_median.R
@@ -1,24 +1,27 @@
-#' Confidence bnterval for median by inverting sign test
+#' Confidence interval for median by inverting sign test
 #' 
 #' @param x vector of data 
-#' @param conf.level level for CI (as decimal), default 95%
-#' @return lower and upper limits 
-#'  
+#' @param conf.level level for CI (as decimal), default 95 perent
+#' @return lower and upper limits
+#' 
 #' @author Ken Butler, \email{butler@utsc.utoronto.ca}
+#' 
 #' @examples 
 #' ci_median(mtcars$mpg,0.90)
 #' ci_median(1:20)
 #' ci_median(1:5)
 #'
-#' @importFrom purrr map_dbl
+#' @importFrom dplyr filter
+#' @importFrom dplyr slice
 #' @export
 #' 
 ci_median=function(x,conf.level=0.95) {
   r=range(x)
   y=seq(from=r[1]-1,to=r[2]+1,length.out = 3*length(x)) # "denser" than x
-  pv=map_dbl(y,pval_sign,x)
+  pv=purrr::map_dbl(y,pval_sign,x)
   d=data.frame(y,pv)
-  lims=d %>% filter(pv>=1-conf.level) %>% slice(c(1,n()))
+  tmp1=dplyr::filter(d,pv>=1-conf.level) 
+  lims=dplyr::slice(tmp1,c(1,dplyr::n()))
   thelims=c(lims[1,1],lims[2,1])
   thelims
 }
