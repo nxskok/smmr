86566a8e738927f7d1767d4c5405ebdad55c0cf3
nxskok <nxskok@gmail.com>
2017-08-11 12:37:39 -0400

    est median by bisection

--- R/ci_median.R
+++ R/ci_median.R
@@ -37,6 +37,7 @@ ci_median0=function(x,conf.level=0.95) {
 #' @param d a data frame
 #' @param x unquoted name of column of data
 #' @param conf.level level for CI (as decimal), default 95 percent
+#' @param tol ends of CI determined to within this accuracy, default 0.01
 #' @return lower and upper limits
 #' 
 #' @author Ken Butler, \email{butler@utsc.utoronto.ca}
@@ -54,19 +55,41 @@ ci_median0=function(x,conf.level=0.95) {
 #'
 #' @export
 #' 
-ci_median=function(d,x,conf.level=0.95) {
+ci_median=function(d,x,conf.level=0.95,tol=0.01) {
   x1=enquo(x)
   exes= d %>% pull(!!x1)
-  r=range(exes)
-  xx=sort(unique(exes))
-  y1=c(r[1]-1,xx)
-  y2=c(xx,r[2]+1)
-  betweens=(y1+y2)/2
-  y=sort(c(xx,betweens))
-  pv=purrr::map_dbl(y,pval_sign,d,!!x1)
-  inside=(pv>=1-conf.level)
-  first=min(which(inside))
-  last=max(which(inside))
-  c(y[first],y[last])
+  below=min(exes)-1
+  med=median(exes)
+  above=max(exes)+1
+  # two bisections, one for each end of the interval
+  # bottom end
+  lo=below
+  hi=med
+  while(abs(hi-lo)>tol) {
+    try=(hi+lo)/2
+    ptry=pval_sign(try,d,!!x1)
+    if (ptry<1-conf.level) {
+      # outside
+      lo=try
+    } else {
+      hi=try
+    }
+  }
+  ci_lo=hi
+  # top end
+  lo=med
+  hi=above
+  while(abs(hi-lo)>tol) {
+    try=(hi+lo)/2
+    ptry=pval_sign(try,d,!!x1)
+    if (ptry<1-conf.level) {
+      # outside
+      hi=try # the other way around because P-values are coming down
+    } else {
+      lo=try
+    }
+  }
+  ci_hi=lo
+  return(c(ci_lo,ci_hi))
 }
 
